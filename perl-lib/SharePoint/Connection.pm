f6c851a21ae65d4f52b67cce57d4a6bcd2ce2948
caldatappd <caldatappd@srv01219.soton.ac.uk>
2015-11-05 13:08:46 +0000

    moved ical and tsv functions into the connection module

--- perl-lib/SharePoint/Connection.pm
+++ perl-lib/SharePoint/Connection.pm
@@ -6,6 +6,7 @@ use Authen::NTLM;
 use SOAP::Lite on_action => sub { "$_[0]$_[1]"; };
 use Data::Dumper;
 use MIME::Base64;
+use Time::Local;
 
 use strict;
 use warnings;
@@ -18,6 +19,7 @@ use warnings;
 # -- site
 # optional
 # -- debug (default: 0)
+# -- domain
 # -- on_error (default: ::die)
 
 sub new
@@ -36,8 +38,14 @@ sub new
 		SOAP::Lite->import(+trace => 'all');
 	}
 
+	my $username = $opts{username};
+	if( defined $opts{domain} )
+	{
+		$username = $opts{domain}."\\".$username;
+	}
+
 	# There's got to be a better way, but this does appear to work!
-	eval "sub SOAP::Transport::HTTP::Client::get_basic_credentials { return ('$opts{username}' => '$opts{password}') };"; 
+	eval "sub SOAP::Transport::HTTP::Client::get_basic_credentials { return ('$username' => '$opts{password}') };"; 
 	
 	return $self;
 }
@@ -256,5 +264,137 @@ sub GetUserCollectionFromGroup
 	return $self->attrFromList( $call->dataof('//GetUserCollectionFromGroup/Users/User') );
 }
 
+####### Higher level conversion functions
+
+sub CalendarAsICAL
+{
+	my( $self, %opts ) = @_;
+
+	my $map = {};
+	my $listinfo = $self->GetList( $opts{list} );
+	foreach my $field ( @{$listinfo->{fields}} )
+	{
+		$map->{$field->{DisplayName}} = "ows_".$field->{StaticName};
+	}
+	# always use real title
+	$map->{"Title"}="ows_Title"; 
+	if( $opts{view} eq "" ) { delete $opts{view}; }
+
+	my @items = $self->GetCalendarEvents( $opts{list}, $opts{view} );
+
+	my @fields = ("Start Time", "End Time", "Title", "Description", "Unique Id");
+
+	my $title = "Sharepoint Calendar";
+	if( defined $opts{title} && $opts{title} ne "" ) { $title=$opts{title}; }
+
+	my @output = ();
+
+	push @output, "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//SOTON//Sharepoint Calendar Fields//EN\r\nMETHOD:PUBLISH\r\nX-WR-CALNAME:$title\r\nX-WR-CALDESC:$title\r\nX-WR-TIMEZONE:Europe/London\r\n";
+
+	foreach my $item ( @items )
+	{
+		push @output, "BEGIN:VEVENT\r\n";
+		my @start = split(/[- :]/, $item->{$map->{'Start Time'}});
+		my @end = split(/[- :]/, $item->{$map->{'End Time'}});
+		# work out if this is a all day event, which sharepoint
+		# exports as 000000 to 235900. If so make it an ics all day
+		if( $start[3].$start[4].$start[5] eq "000000"
+		 && $end[3].$end[4].$end[5] eq "235900" )
+		{
+			my $end_date_t = timelocal(0,0,0,$end[2],$end[1]-1,$end[0]-1900);
+			$end_date_t += 24*60*60;
+			my @end_date = localtime( $end_date_t );
+			push @output, sprintf( "DTSTART;TZID=Europe/London;VALUE=DATE:%04d%02d%02d\r\n", @start );
+			push @output, sprintf( "DTEND;TZID=Europe/London;VALUE=DATE:%04d%02d%02d\r\n", $end_date[5]+1900,$end_date[4]+1,$end_date[3]);
+		}
+		else
+		{
+			push @output, sprintf( "DTSTART;TZID=Europe/London:%04d%02d%02dT%02d%02d%02d\r\n", @start );
+			push @output, sprintf( "DTEND;TZID=Europe/London:%04d%02d%02dT%02d%02d%02d\r\n", @end);
+		}
+		if ( !defined($item->{$map->{'Title'}}) ) { $item->{$map->{'Title'}} = "No Summary"; }
+		push @output, "SUMMARY:".$item->{$map->{'Title'}}."\r\n";
+		if ( !defined($item->{$map->{'Description'}}) ) { $item->{$map->{'Description'}} = "No Description"; }
+		push @output, "DESCRIPTION:".$item->{$map->{'Description'}}."\r\n";
+		if ( defined($item->{$map->{'Location'}}) ) { push @output, "LOCATION:".$item->{$map->{'Location'}}."\r\n"; }
+		push @output, "END:VEVENT\r\n";
+	}
+
+	push @output, "END:VCALENDAR\r\n";
+
+	return join( '', @output );
+}
+
+sub ListAsTSV
+{
+	my( $self, %opts ) = @_;
+
+	my $map = {};
+	my $listinfo = $self->GetList( $opts{list} );
+	my $formats = {};
+	foreach my $field ( @{$listinfo->{fields}} )
+	{
+		$map->{"ows_".$field->{StaticName}} = $field->{DisplayName};
+		$formats->{"ows_".$field->{StaticName}} = $field->{Format} || "none";
+	}
+	if( $opts{view} eq "" ) { delete $opts{view}; }
+
+	my @items = $self->GetListItems( $opts{list}, $opts{view} );
+
+	my @fields;
+	if( defined $opts{'fields-list'} )
+	{
+		@fields  = @{ $opts{"fields-list"} };
+	}
+	if( !scalar @fields )
+	{
+		my $f={};
+		foreach my $item ( @items )
+		{
+			CELL: foreach my $k ( sort keys %{$item} )
+			{
+				next CELL unless defined $map->{$k};
+				$f->{$map->{$k}} = 1;
+			}
+		}
+		@fields = sort keys %$f;
+	}
+
+	my @output = ();	
+	my $row = join( "\t", @fields );
+	$row =~ s/\n/\\n/g;
+	push @output, $row."\n";
+	foreach my $item ( @items )
+	{
+		my $cells = {};
+		CELL: foreach my $k ( sort keys %{$item} )
+		{
+			next CELL unless defined $map->{$k};
+			my $v = $item->{$k};
+			my $name = $map->{$k};
+			$v =~ s/^.*;#//;
+			# url & email fields need more cleaning, but for now:
+			# mailto:hs8@soton.ac.uk, hs8@soton.ac.uk
+			if( $formats->{$k} eq "Hyperlink" )
+			{
+				$v=~s/^([^,]*),.*$/$1/;
+			}
+
+			$cells->{$name} = $v;
+		}
+		my @values = ();
+		foreach my $cell_name ( @fields )
+		{
+			my $v = $cells->{$cell_name};
+			$v = "" unless defined $v;
+			push @values, $v;
+		}
+		my $row = join( "\t", @values );
+		$row =~ s/\n/\\n/g;
+		push @output, $row."\n";
+	}
+
+	return join( '', @output );
+}
 
 1;
